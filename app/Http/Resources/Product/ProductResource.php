6b8b4a209d5c24ba4f5b09a29d8a84596c17d0d0
mnm <mohamednabih22022014@gmail.com>
2018-12-03 15:24:45 +0200

    Modify Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -20,9 +20,14 @@ class ProductResource extends Resource
             
             'name' => $this->name,
             'detail' => $this->detail,
+            'stock' => $this->stock== 0 ? "Out Of Stock": $this->stock,
             'price' => $this->price,
-            'stock' => $this->stock,
             'discount' => $this->discount,
+            'netPrice' => round((1- $this->discount/100) * $this->price , 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'Not Rating Yet',
+            'href' => [
+                'review' => route('reviews.index',$this->id)
+            ],
         ];
     }
 }
