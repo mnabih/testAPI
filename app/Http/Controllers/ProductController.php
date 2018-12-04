f7c9c2b7a17a3151671369eecbead4fd85923c8c
mnm <mohamednabih22022014@gmail.com>
2018-12-04 10:08:43 +0200

    Create product & use validator instead of request roles

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,9 @@ use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Response;
 
 class ProductController extends Controller
 {
@@ -37,7 +40,29 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $Validator = Validator::make($request->all(), [
+
+            'name'          => 'required|max:255|unique:products',
+            'description'   => 'required',
+            'price'         => 'required|max:10',
+            'stock'         => 'required|max:6',
+            'discount'      => 'required|max:2',
+
+        ]);
+
+        if($Validator->passes()) {        
+            $product = new Product;
+            $product->name = $request->name;
+            $product->detail = $request->description;
+            $product->stock = $request->stock;
+            $product->price = $request->price;
+            $product->discount = $request->discount;
+            $product->save();
+            return response([
+                'data' => new ProductResource($product)
+            ],201);
+        }return response()->json(['key'=> 'error', 'value'=> 0 ,'data'=> $Validator->errors()]);
     }
 
     /**
